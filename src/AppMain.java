5b1de63e48c43dfdde00dd2b5324e68add4a314e
JeTSkY1h <jetsky1h@googlemail.com>
2022-05-17 15:39:08 +0200

    Task3

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,7 +1,22 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
-        for (String s: args) {
-            System.out.println(s);
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("first number: ");
+        int wertA = keyboard.nextInt();
+        System.out.print("Second number: ");
+        int wertB = keyboard.nextInt();
+
+        if (wertA == wertB) {
+            System.out.println("Die zahlen sind gleich Groß");
         }
+        if(wertA < wertB) {
+            System.out.println("Zahl 1 ist Kleiner");
+        } else {
+            System.out.println("Zahl 1 ist größer");
+        }
+
     }
 }
